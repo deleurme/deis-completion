52939b601729ad5f03e27213d0e3daee215cda9a
Shawn Deleurme <shawn@deleur.me>
2015-11-06 16:22:24 -0800

    Fixed the install script

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 OS=`uname`
 
 if [ $OS == "Darwin" -a -x `which brew` ]; then
-  cp deis_completion /usr/local/etc/bash_completion.d
+  cp deis-completion /usr/local/etc/bash_completion.d
 else 
-  cp deis_completion /etc/bash_completion.d/
+  cp deis-completion /etc/bash_completion.d/
 fi
