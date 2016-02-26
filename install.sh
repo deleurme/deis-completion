11b740f81c47cc03089e302f2389a4005129c992
Shawn Deleurme <shawn@deleur.me>
2016-02-26 11:12:43 -0800

    installer fixes

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 
 OS=`uname`
 
-if [ $OS == "Darwin" -a -x `which brew` ]; then
+if [ $OS == "Darwin" ] && [ -x $(which brew) ]; then
   cp deis-completion /usr/local/etc/bash_completion.d
 else 
   cp deis-completion /etc/bash_completion.d/
